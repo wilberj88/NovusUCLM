648c984130f4d0edf5b63491b76e280d89b9f02e
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-10-03 09:53:14 -0500

    Update redes.py

--- pages/redes.py
+++ pages/redes.py
@@ -6,4 +6,4 @@ st.title('Hola, soy Atento 🤖')
 a = st.text_input('¿Cuál es el perfil que quieres analizar?')
 
 if a:
-  st.write('Hola, ya estoy investigando sobre ',a, 'pronto te enviaré mi análisis de sentimiento 🤖')
+  st.write('Hola, ya estoy investigando sobre ',a, 'pronto te enviaré mi análisis de sentimiento 🤖 por cada red social donde aparezca')
