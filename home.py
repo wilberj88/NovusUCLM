c3b803342cba13ffc8708b3c1dfdf5e1b8aa85c6
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-10-03 09:40:18 -0500

    Create home.py

---
+++ home.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Novus Piloto con UCLM", page_icon="🤖")
+
+st.title('Novus Piloto con UCLM 🤖')
+st.header("Transferencia de Conocimiento 🎯")
+
+st.write("Alternativas de trabajo conjunto👋")
+
+st.markdown(
+  """
+Detallamos dos alternativas basadas en análisis de sentimientos:
+  - 🔎 _    Visión Computacional de Rostros
+  - 🛒 _    Ingesta de Datos de Redes Sociales por tipos de perfiles
+  
+  VEAMOS 2 EJEMPLOS AHORA 🕰
+  """
+)
