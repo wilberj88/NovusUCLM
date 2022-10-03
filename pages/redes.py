0cf297158bfc65106dcd4a0ef81208442e2431cb
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-10-03 09:50:33 -0500

    Create redes.py

---
+++ pages/redes.py
@@ -0,0 +1,9 @@
+
+
+import streamlit as st
+
+st.title('Hola, soy Atento 🤖')
+a = st.text_input('¿Cuál es el perfil que quieres analizar?')
+
+if a:
+  st.write('Hola, ya estoy investigando sobre ',a, 'pronto te enviaré mi análisis de sentimiento 🤖')
